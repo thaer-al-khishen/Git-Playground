8f014f6bcddc6dddd026b2284a5a56febb16f0e0
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2024-03-16 21:51:45 +0200

    Initial setup

---
+++ app/src/main/java/com/relatablecode/gitplaygroundapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.relatablecode.gitplaygroundapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
