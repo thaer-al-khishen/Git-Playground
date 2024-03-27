ab95bed236bf3672b4b5981b40ee8b395503e3fb
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2024-03-27 22:48:32 +0200

    Second feature

--- app/src/main/java/com/relatablecode/gitplaygroundapplication/MainActivity.kt
+++ app/src/main/java/com/relatablecode/gitplaygroundapplication/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //News feature
+        //Second feature
     }
 }
